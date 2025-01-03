9ae5ab82165202bb76d0f8149eb6f4f56cff9881
Renzo Gregorio <renzogregorio0517@gmail.com>
2025-01-03 17:31:33 +0800

    footer scratch

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,28 @@
 <div class="absolute -z-10 mt-32 flex w-full bg-darkblue h-auto py-4 shadow-inner">
-    <img src="{{ asset('images/logo.png') }}" class="w-1/3">
+    <div class="w-1/3">
+        <img src="{{ asset('images/logo.png') }}" class="w-2/3 mx-auto">
+    </div>
+    <div class="w-2/3 flex justify-around">
+        <div class="flex flex-col text-white">
+            <h3 class="font-bold mb-2">Column 1</h3>
+            <a href="#">Sample Text 1</a>
+            <a href="#">Sample Text 2</a>
+            <a href="#">Sample Text 3</a>
+            <a href="#">Sample Text 4</a>
+        </div>
+        <div class="flex flex-col text-white">
+            <h3 class="font-bold mb-2">Column 2</h3>
+            <a href="#">Sample Text 1</a>
+            <a href="#">Sample Text 2</a>
+            <a href="#">Sample Text 3</a>
+            <a href="#">Sample Text 4</a>
+        </div>
+        <div class="flex flex-col text-white">
+            <h3 class="font-bold mb-2">Column 3</h3>
+            <a href="#">Sample Text 1</a>
+            <a href="#">Sample Text 2</a>
+            <a href="#">Sample Text 3</a>
+            <a href="#">Sample Text 4</a>
+        </div>
+    </div>
 </div>
