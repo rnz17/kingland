e80774e242c80f34fe370eb795e073010a2cc034
Renzo Gregorio <renzogregorio0517@gmail.com>
2025-03-02 17:02:06 +0800

    [deploy] products debug

--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -170,6 +170,7 @@
         btn.addEventListener('click', () => {
             const products = @json($products);
             var code = btn.id;
+            console.log(products);
             for (let product of products){
                 if (product.code == code){
                     item = product;
